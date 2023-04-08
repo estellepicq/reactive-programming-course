8c1faac1a35aecdab30bb642be93e30bf02ed7ee
PICQ Estelle <estelle.picq@altran.com>
2023-04-08 10:38:20 +0200

    chore: remove code samples

--- code-samples/1_imperative-code.ts
+++
@@ -1,9 +0,0 @@
-class ImperativeClass {
-    transform() {
-        const numbers = [1, 2, 3];
-        for (let i = 0; i < numbers.length; i++) {
-            numbers[i] = numbers[i] + 1;
-        }
-        return numbers;
-    }
-}
--- code-samples/1_reactive-code.ts
+++
@@ -1,6 +0,0 @@
-class ReactiveClass {
-    transform() {
-        const numbers = [1, 2, 3];
-        return numbers.map(number => number + 1);
-    }
-}
